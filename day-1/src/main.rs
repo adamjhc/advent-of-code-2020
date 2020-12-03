0784763dbe1414540e963c2739ffb8cc0c596c8c
Adam Cox <a.cox@kainos.com>
2020-12-03 09:03:00 +0000

    Add part 2 implementation

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -3,6 +3,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
     let mut years = Vec::new();
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines {
@@ -16,6 +21,28 @@ fn main() {
             for j in 0..year_len {
                 if years[i] + years[j] == 2020 {
                     println!("{}", years[i] * years[j]);
+                }
+            }
+        }
+    }
+}
+
+fn part_2() {
+    let mut years = Vec::new();
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(year) = line {
+                years.push(year.parse::<i32>().unwrap());
+            }
+        }
+
+        let year_len = years.len();
+        for i in 0..year_len {
+            for j in 0..year_len {
+                for k in 0..year_len {
+                    if years[i] + years[j] + years[k] == 2020 {
+                        println!("{}", years[i] * years[j] * years[k]);
+                    }
                 }
             }
         }
