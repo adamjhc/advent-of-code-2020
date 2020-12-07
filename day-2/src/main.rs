295c7b6943bbf69d3c4e0c5817c092d64c4c3f16
Adam Cox <a.cox@kainos.com>
2020-12-07 11:51:29 +0000

    Complete day 2 part 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -29,6 +30,29 @@ fn part_1() {
         }
 
         if count >= min && count <= max {
+            valid += 1;
+        }
+    }
+
+    println!("{}", valid);
+}
+
+fn part_2() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+    let re = Regex::new(r"^(\d{1,2})-(\d{1,2}) (.): (.*)$").unwrap();
+
+    let mut valid = 0;
+    for line in lines {
+        let l = line.unwrap();
+        let caps = re.captures(&l).unwrap();
+
+        let pos1 = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
+        let pos2 = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        let cha = caps.get(3).unwrap().as_str().parse::<char>().unwrap();
+        let pas = caps.get(4).unwrap().as_str();
+
+        if (pas.chars().nth(pos1 - 1).unwrap() == cha) ^ (pas.chars().nth(pos2 - 1).unwrap() == cha) {
             valid += 1;
         }
     }
