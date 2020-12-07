c98fc3db59d1b3852db213e66f99470ea7148902
Adam Cox <a.cox@kainos.com>
2020-12-07 10:55:42 +0000

    Complete day 2 part 1

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,39 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    part_1().unwrap();
+}
+
+fn part_1() -> Result<(), Error> {
+    let file = File::open("./input.txt")?;
+    let lines = BufReader::new(file).lines();
+    let re = Regex::new(r"^(\d{1,2})-(\d{1,2}) (.): (.*)$").unwrap();
+
+    let mut valid = 0;
+    for line in lines {
+        let l = line?;
+        let caps = re.captures(&l).unwrap();
+
+        let min = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let max = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        let cha = caps.get(3).unwrap().as_str().parse::<char>().unwrap();
+        let pas = caps.get(4).unwrap().as_str();
+
+        let mut count = 0;
+        for ch in pas.chars() {
+            if ch == cha {
+                count += 1;
+            }
+        }
+
+        if count >= min && count <= max {
+            valid += 1;
+        }
+    }
+
+    println!("{}", valid);
+
+    Ok(())
 }
