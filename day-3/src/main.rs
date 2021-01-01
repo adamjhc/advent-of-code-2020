6272ffb8db2da61e4ba0f2ae094b0f5f87696a9b
Adam Cox <a.cox@kainos.com>
2021-01-01 16:17:59 +0000

    Complete Day 3 Part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,42 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    part_1();
+}
+
+fn part_1() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+
+    let mut map: Vec<Vec<char>> = Vec::new();
+    for (i, line) in lines.enumerate() {
+        map.push(Vec::new());
+        let l = line.unwrap();
+        for chr in l.chars() {
+            map[i].push(chr)
+        }
+    }
+
+    let mut trees = 0;
+
+    let right = 3;
+    let down = 1;
+
+    let mut row = 0;
+    let mut col = 0;
+
+    let col_size = map[0].len();
+    while row < map.len() {
+        if map[row][col] == '#' {
+            trees += 1;
+        }
+
+        row += down;
+
+        col += right;
+        col %= col_size;
+    }
+
+    println!("{}", trees)
 }
