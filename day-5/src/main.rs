e82428eb97fdbf1bf2ffcc9d91c002ee376e4ca4
Adam Cox <a.cox@kainos.com>
2021-01-05 14:59:08 +0000

    Complete Day 5 Part 1

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,3 +1,63 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    part_1();
+}
+
+fn part_1() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+
+    let mut highest_seat_id = 0;
+    for line in lines {
+        let line = line.unwrap();
+        let mut chars = line.chars();
+
+        // calculate row
+        let mut lower = 0;
+        let mut upper = 127;
+        for _ in 0..6 {
+            let row_char = chars.next().unwrap();
+            let middle = (lower + upper) / 2;
+            if row_char == 'F' {
+                upper = middle;
+            } else if row_char == 'B' {
+                lower = middle + 1;
+            }
+        }
+
+        let row = if chars.next().unwrap() == 'F' {
+            lower
+        } else {
+            upper
+        };
+
+        // calculate column
+        let mut lower = 0;
+        let mut upper = 7;
+        for _ in 8..10 {
+            let column_char = chars.next().unwrap();
+            let middle = (lower + upper) / 2;
+            if column_char == 'L' {
+                upper = middle;
+            } else if column_char == 'R' {
+                lower = middle + 1;
+            }
+        }
+
+        let column = if chars.next().unwrap() == 'L' {
+            lower
+        } else {
+            upper
+        };
+
+        let seat_id = row * 8 + column;
+
+        if seat_id > highest_seat_id {
+            highest_seat_id = seat_id
+        }
+    }
+
+    println!("{}", highest_seat_id)
 }
