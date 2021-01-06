4ba5d22531006002e44e849ed9464d1aec9a96a9
Adam Cox <a.cox@kainos.com>
2021-01-06 12:09:29 +0000

    Complete Day 6 Part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -18,6 +19,31 @@ fn part_1() {
                 }
             }
         }
+    }
+
+    println!("{}", sum);
+}
+
+fn part_2() {
+    let batch = fs::read_to_string("./input.txt").unwrap();
+    let mut sum = 0;
+    for passport in batch.split("\n\n").map(str::to_owned) {
+        let mut question_intersection: Vec<char> = Vec::new();
+
+        for (i, line) in passport.lines().enumerate() {
+            if i == 0 {
+                for question in line.chars() {
+                    question_intersection.push(question);
+                }
+            } else {
+                question_intersection = question_intersection
+                    .into_iter()
+                    .filter(|&x| line.find(x) != None)
+                    .collect();
+            }
+        }
+
+        sum += question_intersection.len();
     }
 
     println!("{}", sum);
