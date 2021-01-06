f02c028a9857eb06f79f0fe7e663a9d4e25bdb48
Adam Cox <a.cox@kainos.com>
2021-01-06 09:44:27 +0000

    Complete Day 6 Part 1

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    part_1();
+}
+
+fn part_1() {
+    let batch = fs::read_to_string("./input.txt").unwrap();
+    let mut sum = 0;
+    for passport in batch.split("\n\n").map(str::to_owned) {
+        let mut unique_questions = String::new();
+
+        for line in passport.lines() {
+            for question in line.chars() {
+                if !unique_questions.contains(question) {
+                    unique_questions.push(question);
+                    sum += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", sum);
 }
