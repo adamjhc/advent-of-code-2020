3622e36f7a498d826f44abb85189a7944fd914f9
Adam Cox <a.cox@kainos.com>
2021-01-14 16:09:06 +0000

    Complete Day 8 Part 1

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,3 +1,51 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    part_1();
+}
+
+fn part_1() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = BufReader::new(file).lines().map(|l| l.unwrap());
+    let code: Vec<String> = lines.collect();
+
+    let mut program_counter = 0;
+    let mut lines_run: Vec<i32> = Vec::new();
+    let mut accumulator = 0;
+
+    while !lines_run.contains(&program_counter) {
+        lines_run.push(program_counter);
+
+        // parse line of code
+        let line = code.get(program_counter as usize).unwrap();
+        let instruction = &line[0..3];
+        match instruction {
+            "acc" => {
+                let sign = line.chars().nth(4).unwrap();
+                let amount = &line[5..].parse::<i32>().unwrap();
+
+                if sign == '+' {
+                    accumulator += amount
+                } else if sign == '-' {
+                    accumulator -= amount
+                }
+
+                program_counter += 1
+            }
+            "jmp" => {
+                let sign = line.chars().nth(4).unwrap();
+                let amount = &line[5..].parse::<i32>().unwrap();
+
+                if sign == '+' {
+                    program_counter += amount
+                } else if sign == '-' {
+                    program_counter -= amount
+                }
+            }
+            _ => program_counter += 1,
+        }
+    }
+
+    println!("{}", accumulator)
 }
