f1642e00f1389a6c0561b32ced01a3a2b871af7e
Adam Cox <a.cox@kainos.com>
2021-01-14 10:27:04 +0000

    Complete Day 7 Part 2

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -5,6 +5,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -48,5 +49,49 @@ fn count_bag_colours(bags: &HashMap<String, Vec<String>>, bag_inside: &str) -> H
             bag_colours
         }
         None => HashSet::new(),
+    }
+}
+
+fn part_2() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = BufReader::new(file).lines().map(|l| l.unwrap());
+
+    // build up graph of bags
+    let mut bags: HashMap<String, Vec<(i32, String)>> = HashMap::new();
+    let rule_regex = Regex::new(r"(\w+ \w+) bags contain (.*)\.").unwrap();
+
+    for line in lines {
+        let rule_captures = rule_regex.captures(&line).unwrap();
+        let bag_outside = rule_captures.get(1).unwrap().as_str();
+
+        let contains_regex = Regex::new(r"(?:(\d) (\w+ \w+))").unwrap();
+        let contains = rule_captures.get(2).unwrap().as_str();
+        let contains_captures = contains_regex.captures_iter(&contains);
+        for cap in contains_captures {
+            let bag_inside_count = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let bag_inside = cap.get(2).unwrap().as_str();
+
+            if bags.contains_key(bag_outside) {
+                bags.get_mut(bag_outside)
+                    .unwrap()
+                    .push((bag_inside_count, bag_inside.to_string()))
+            } else {
+                bags.insert(
+                    bag_outside.to_string(),
+                    vec![(bag_inside_count, bag_inside.to_string())],
+                );
+            }
+        }
+    }
+
+    println!("{}", count_bags_inside(&bags, "shiny gold"));
+}
+
+fn count_bags_inside(bags: &HashMap<String, Vec<(i32, String)>>, bag_outside: &str) -> i32 {
+    match bags.get(bag_outside) {
+        Some(bags_inside) => bags_inside.iter().fold(0, |count, bag| {
+            count + bag.0 + bag.0 * count_bags_inside(bags, &bag.1)
+        }),
+        None => 0,
     }
 }
