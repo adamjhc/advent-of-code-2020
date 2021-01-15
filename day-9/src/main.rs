8bc0885b3cfe88cace87002ecbc4ebff67d77c44
Adam Cox <a.cox@kainos.com>
2021-01-15 10:34:17 +0000

    Complete Day 9 Part 2

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -2,16 +2,17 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    part_1();
-}
-
-fn part_1() {
     let file = File::open("./input.txt").unwrap();
     let lines: Vec<i64> = BufReader::new(file)
         .lines()
         .map(|l| l.unwrap().parse::<i64>().unwrap())
         .collect();
 
+    let invalid_number = part_1(&lines);
+    part_2(&lines, invalid_number);
+}
+
+fn part_1(lines: &Vec<i64>) -> i64 {
     for i in 25..lines.len() {
         let mut found_pair = false;
         'calc: for j in 0..25 {
@@ -29,7 +30,36 @@ fn part_1() {
 
         if !found_pair {
             println!("{}", lines[i]);
-            return;
+            return lines[i];
+        }
+    }
+
+    0
+}
+
+fn part_2(lines: &Vec<i64>, invalid_number: i64) {
+    for i in 0..lines.len() {
+        let mut sum = lines[i];
+        let mut smallest = lines[i];
+        let mut largest = lines[i];
+        for j in i + 1..lines.len() {
+            sum += lines[j];
+
+            if lines[j] < smallest {
+                smallest = lines[j]
+            }
+            if lines[j] > largest {
+                largest = lines[j]
+            }
+
+            if sum == invalid_number {
+                println!("{}", smallest + largest);
+                return;
+            }
+
+            if sum > invalid_number {
+                break;
+            }
         }
     }
 }
