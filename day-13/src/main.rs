9df805b238661ec06d6d1f1159305cd4abd347b8
Adam Cox <a.cox@kainos.com>
2021-01-21 11:32:00 +0000

    Completed Day 13 Part 1

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    part_1();
+}
+
+fn part_1() {
+    let file = File::open("./input.txt").unwrap();
+    let mut lines = BufReader::new(file).lines();
+
+    let earliest_time = lines.next().unwrap().unwrap().parse::<i32>().unwrap();
+    let buses: Vec<i32> = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(',')
+        .into_iter()
+        .filter(|s| s != &"x")
+        .map(|t| t.parse::<i32>().unwrap())
+        .collect();
+
+    let mut current_time = earliest_time;
+    loop {
+        for bus_time in &buses {
+            if current_time % bus_time == 0 {
+                println!("{}", bus_time * (current_time - earliest_time));
+                return;
+            }
+        }
+
+        current_time += 1;
+    }
 }
