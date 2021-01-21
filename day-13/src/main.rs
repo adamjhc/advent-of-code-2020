9663ca992a91b1fe1a4d6848a9053fa5a8d7e4f7
Adam Cox <a.cox@kainos.com>
2021-01-21 14:20:42 +0000

    Completed Day 13 Part 2
    Really didn't know what I was doing for this one and mostly copied
    it from https://dev.to/benwtrent/comment/1962m

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -31,4 +32,44 @@ fn part_1() {
 
         current_time += 1;
     }
+}
+
+fn part_2() {
+    let file = File::open("./input.txt").unwrap();
+    let mut lines = BufReader::new(file).lines();
+
+    // ignore first line
+    lines.next();
+
+    let mut bus_no = -1;
+    let buses: Vec<(i64, i64)> = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(',')
+        .into_iter()
+        .filter_map(|bus| {
+            bus_no += 1;
+            if let Ok(bus_id) = bus.parse::<i64>() {
+                return Some((bus_no, bus_id));
+            }
+            None
+        })
+        .collect();
+
+    // for some reason the sort isn't needed?
+    // buses.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    let mut time: i64 = 0;
+    let mut inc = buses[0].1;
+    for (bus_no, bus_time) in &buses[1..] {
+        // (t + i) % bus[i] = 0
+        while (time + bus_no) % bus_time != 0 {
+            time += inc;
+        }
+
+        // adjust for the next modulo
+        inc *= bus_time;
+    }
+
+    println!("{}", time);
 }
