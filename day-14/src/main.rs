631a8620ce836cd714d7699d1c2a5ad2741ece16
Adam Cox <a.cox@kainos.com>
2021-01-22 16:34:11 +0000

    Complete Day 14 Part 2

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -5,6 +5,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -57,6 +58,59 @@ fn part_1() {
             mem_val &= mask_zeroes;
 
             memory.insert(mem_loc, mem_val);
+        } else {
+            panic!();
+        }
+    }
+
+    println!("{}", memory.iter().fold(0, |sum, (_, value)| sum + value))
+}
+
+fn part_2() {
+    let file = File::open("./input.txt").unwrap();
+    let lines = BufReader::new(file).lines().map(Result::unwrap);
+
+    let mut memory: HashMap<u64, u64> = HashMap::new();
+    let mut mask_ones = 0b0;
+    let mut masks_floating: Vec<u64> = Vec::new();
+
+    for line in lines {
+        if &line[..4] == "mask" {
+            let mask_new = line[7..].to_string();
+
+            // convert to binary
+            mask_ones = 0b0;
+            masks_floating = Vec::new();
+            masks_floating.push(0b0);
+            for bit in mask_new.chars() {
+                if bit == '1' {
+                    mask_ones |= 0b1;
+                } else if bit == 'X' {
+                    // clone existing masks
+                    // add 0 to first, 1 to second
+                    let masks_dupe = masks_floating.clone();
+                    masks_floating.iter_mut().for_each(|mask| *mask |= 0b1);
+                    masks_floating.extend(masks_dupe);
+                }
+
+                mask_ones <<= 1;
+                masks_floating.iter_mut().for_each(|mask| *mask <<= 1);
+            }
+
+            mask_ones >>= 1;
+            masks_floating.iter_mut().for_each(|mask| *mask >>= 1);
+        } else if &line[..3] == "mem" {
+            let re = Regex::new(r"^mem\[(\d+)\] = (\d+)$").unwrap();
+            let captures = re.captures(&line).unwrap();
+
+            let mut mem_loc = captures.get(1).unwrap().as_str().parse::<u64>().unwrap();
+            let mem_val = captures.get(2).unwrap().as_str().parse::<u64>().unwrap();
+
+            // combine mem_val with masks
+            mem_loc |= mask_ones;
+            masks_floating.iter().for_each(|mask| {
+                memory.insert(mem_loc ^ mask, mem_val);
+            });
         } else {
             panic!();
         }
