58f8884b15125371ce615909afe805eab179a174
Adam Cox <adamjhcox@gmail.com>
2021-03-28 19:56:20 +0100

    Start refactoring and adding tests

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,56 +1,75 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{BufRead, BufReader, Lines, Result};
 use std::path::Path;
 
 fn main() {
-    part_1();
-    part_2();
+    let years = read_input();
+
+    println!("{}", part_1(&years));
+    println!("{}", part_2(&years));
 }
 
-fn part_1() {
-    let mut years = Vec::new();
-    if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {
-            if let Ok(year) = line {
-                years.push(year.parse::<i32>().unwrap());
+fn part_1(years: &Vec<i32>) -> i32 {
+    let year_len = years.len();
+    for i in 0..year_len {
+        for j in 0..year_len {
+            if years[i] + years[j] == 2020 {
+                return years[i] * years[j];
             }
         }
+    }
 
-        let year_len = years.len();
-        for i in 0..year_len {
-            for j in 0..year_len {
-                if years[i] + years[j] == 2020 {
-                    println!("{}", years[i] * years[j]);
+    panic!()
+}
+
+fn part_2(years: &Vec<i32>) -> i32 {
+    let year_len = years.len();
+    for i in 0..year_len {
+        for j in 0..year_len {
+            for k in 0..year_len {
+                if years[i] + years[j] + years[k] == 2020 {
+                    return years[i] * years[j] * years[k];
                 }
             }
         }
     }
+
+    panic!()
 }
 
-fn part_2() {
+fn read_input() -> Vec<i32> {
     let mut years = Vec::new();
-    if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {
-            if let Ok(year) = line {
-                years.push(year.parse::<i32>().unwrap());
-            }
-        }
-
-        let year_len = years.len();
-        for i in 0..year_len {
-            for j in 0..year_len {
-                for k in 0..year_len {
-                    if years[i] + years[j] + years[k] == 2020 {
-                        println!("{}", years[i] * years[j] * years[k]);
-                    }
-                }
-            }
-        }
+    let lines = read_lines("./input.txt").expect("./input.txt not found");
+    for line in lines {
+        years.push(line.unwrap().parse::<i32>().unwrap());
     }
+
+    years
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_gives_correct_answer() {
+        let years = read_input();
+
+        assert_eq!(part_1(&years), 1006875)
+    }
+
+    #[test]
+    fn part_2_gives_correct_answer() {
+        let years = read_input();
+
+        assert_eq!(part_2(&years), 165026160)
+    }
 }
